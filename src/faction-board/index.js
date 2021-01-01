a812828b2b9b15a3125cb57bf9768f5cc61246b5
mrjadaml <mrjadaml@gmail.com>
2021-01-01 08:07:57 -0800

    faction board reputation tracker

--- src/faction-board/index.js
+++ src/faction-board/index.js
@@ -5,6 +5,8 @@ import Buildings from '../buildings/';
 import Satellite from '../satellite/';
 
 const FactionBoard = () => {
+  const reputation = [10,9,8,7,6,5,4,3,2,1,0,-1,-2,-3,-4,-5,-6,-7,-8,-9,-10];
+
   return (
     <div className={styles.board}>
       <div className={styles.header}>
@@ -17,6 +19,14 @@ const FactionBoard = () => {
       </div>
 
       <div className={styles.body}>
+        <div className={styles.reputationTrack}>
+          {reputation.map((value) => (
+            <div className={styles.reputation}>
+              {value}
+            </div>
+          ))}
+        </div>
+
         <div className={styles.side}>
           <div className={styles.abilities}>
             Faction Abilities...
@@ -25,7 +35,9 @@ const FactionBoard = () => {
             <Buildings />
           </div>
         </div>
+
         <HexMap />
+
         <div className={styles.satellite}>
           <Satellite />
         </div>
